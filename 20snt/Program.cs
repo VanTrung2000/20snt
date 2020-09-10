07639242149afeccda30fdfbe4dbeff6a33e67b3
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-10 20:58:28 +0700

    Add project files.

---
+++ 20snt/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _20snt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 20;
+            Console.Write($"{n} so nguyen to dau tien la :");
+            daySNT(n);
+            Console.ReadLine();
+        }
+        static void daySNT(int n)
+        {
+            int index = 0;
+            for (int i = 2; i < 200; i++)
+            {
+                bool check = true;
+                for (int count = 2; count <= Math.Sqrt(i); count++)
+                {
+                    if (i % count == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                {
+                    index = index + 1;
+                    if (index <= n)
+                    {
+                        Console.Write($"{i} ,");
+                    }
+                }
+            }
+        }
+    }
+}
